9be11652957838b0bd421ad95d6bab6f2571b9e0
ben byford <benjaminbyford@gmail.com>
2012-12-02 22:56:10 +0000

    delete and callbacks
    statement to check for how many times you've been to the site

--- firstTime.js
+++ firstTime.js
@@ -1,5 +1,8 @@
-function firstTime(){
+var FTvalue = 0;
 
+function firstTime(){
+	var firstTimeValue = 0;
+	
 	function FTstorageCheck(){
 	/* check for html5 storage */
 	  try {
@@ -15,16 +18,29 @@ function firstTime(){
 		
 	function FTinit() {
 		if(FTstorageCheck()){
+			
+			function increment(){
+				firstTimeValue++;
+				localStorage.setItem('firstTime', firstTimeValue);
+				
+			}
+			
 			/* check if you've been to the site before */
 			if(localStorage.getItem('firstTime')){
 				/* previously set */
-				alert('set');
-			}else {
+				firstTimeValue = localStorage.getItem('firstTime');
+				FTvalue = firstTimeValue;
+				increment();
+			}else{
 				/* not yet set */
-				localStorage.setItem('firstTime', '1')
-				alert('not set');
+				FTvalue = firstTimeValue;
+				increment();
 			}
 		}
 	}
 	FTinit();
 }
+function firstTimeDelete(){
+	delete localStorage['firstTime'];
+}
+
